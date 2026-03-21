e8cf07ca2042394e7be718c7e2c2370b51628b51
malachyfernandez <jag_pyloric.0j@icloud.com>
2026-03-21 14:31:35 -0500

    Done feed but not mobile

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -5,23 +5,27 @@ import PoppinsText from './ui/text/PoppinsText';
 import FakeConvexWrapper from './Post/FakeConvexWrapper';
 import { PostType } from 'types/postTypes';
 import Template from './Post/Templates/Template';
+import Row from './layout/Row';
+import Column from './layout/Column';
+
 
 const Feed = () => {
     const posts = useUserListGet<PostType>({
         key: "posts",
         // userIds: [], // Get all posts for now
     });
-    
+
     return (
-        <View className='bg-l'>
+        <View className='overflow-scroll h-full'>
             <PoppinsText weight='medium' color='white'>Feed</PoppinsText>
-            {posts?.map((post: any, index: number) => {
-                const text = post?.value?.text ?? 'NO TEXT';
-                const postId = post?.id ?? '?';
-                const userId = post?.userToken ?? '??';
-                return (
-                    <View key={index} style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc' }}>
-                        <PoppinsText style={{ fontSize: 12, color: '#666' }} weight='medium' color='white'>
+            <Column>
+                {posts?.map((post: any, index: number) => {
+                    const text = post?.value?.text ?? 'NO TEXT';
+                    const postId = post?.id ?? '?';
+                    const userId = post?.userToken ?? '??';
+                    return (
+                        <Column key={index}>
+                            {/* <PoppinsText style={{ fontSize: 12, color: '#666' }} weight='medium' color='white'>
                             User: {userId}
                         </PoppinsText>
                         <PoppinsText style={{ marginTop: 5 }} weight='medium' color='white'>
@@ -29,13 +33,15 @@ const Feed = () => {
                         </PoppinsText>
                         <PoppinsText style={{ marginTop: 5 }} weight='medium' color='white'>
                             {`post id: ${postId}`}
-                        </PoppinsText>
+                        </PoppinsText> */}
 
-                        {/* <FakeConvexWrapper /> */}
-                        <Template post={post.value} />
-                    </View>
-                );
-            })}
+                            {/* <FakeConvexWrapper /> */}
+                            <Template post={post.value} />
+
+                        </Column>
+                    );
+                })}
+            </Column>
         </View>
     );
 };
