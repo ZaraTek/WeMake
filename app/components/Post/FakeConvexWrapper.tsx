2142fd1b659b52b780e71bf461f417825638d1d4
malachyfernandez <jag_pyloric.0j@icloud.com>
2026-03-21 13:43:36 -0500

    other

--- app/components/Post/FakeConvexWrapper.tsx
+++ app/components/Post/FakeConvexWrapper.tsx
@@ -1,21 +1,14 @@
 import { useState } from "react";
 import Template from "./Templates/Template";
+import type { PostType } from "./postTypes";
 
 
 const FakeConvexWrapper = () => {
 // use state for the font size
-    const [post] = useState<{
-        postTemplate: "Image" | "Text" | "Video" | "Audio";
-        TemplateData: {
-            ImageUrl: string;
-            Title: string;
-            Subtitle: string;
-        };
-        writeUpData: string;
-    }>({
+    const [post] = useState<PostType>({
         postTemplate: 'Image',
         TemplateData: {
-            ImageUrl: 'https://placehold.co/600x400',
+            ImageUrl: ['https://placehold.co/600x400'],
             Title: 'Title',
             Subtitle: 'Subtitle',
         },
