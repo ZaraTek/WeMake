dbbf5ed21cba6e01d98b7e88001598da4d5083be
Zaratek <zara.tekmen@gmail.com>
2026-03-21 20:54:31 -0400

    enum image templates

--- types/postTypes.ts
+++ types/postTypes.ts
@@ -29,7 +29,7 @@ export type ImagePost = {
   postTemplate: "Image";
   TemplateData: ImageTemplateData;
   writeUpData?: string;
-  imageTemplateVersion: string;
+  imageTemplateVersion: "collage" | "slideshow";
 };
 
 export type AudioPost = {
